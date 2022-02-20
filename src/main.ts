aad6ac2caa6d4ec02f15e7e6196ddda72ad05a80
tohsaka888 <156132264@qq.com>
2022-02-20 11:30:22 +0800

    feat: 使用hash router

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import routes from "../config/router.config";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const app = createApp(App);
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
