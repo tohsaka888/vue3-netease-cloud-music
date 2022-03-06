7583b3b0a2017655a2104e37d1a34ab6be84dbc7
tohsaka888 <156132264@qq.com>
2022-03-06 15:26:12 +0800

    feat: navbar

---
+++ src/components/Navbar/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div>111</div>
+  </div>
+</template>
+  
+<script setup lang='ts'>
+
+</script>
+  
+<style>
+.container {
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: black;
+}
+</style>
