65dc4307a21f8fe6a2b98464551a5ba9e137c37a
tohsaka888 <156132264@qq.com>
2022-03-07 21:09:57 +0800

    feat: 优化actions

--- store/modules/CurrentMusic.ts
+++ store/modules/CurrentMusic.ts
@@ -18,7 +18,10 @@ const state: State = {
 const mutations: MutationTree<State> = {
   // 设置当前音乐信息
   setCurrentMusicInfo(state, payload: { song: Music }) {
-    state.currentMusicInfo = payload.song;
+    state.currentMusicInfo = { ...state.currentMusicInfo, ...payload.song };
+  },
+  setCurrentMusicUrl(state, payload: { url: string }) {
+    state.currentMusicInfo.url = payload.url;
   },
 };
 
@@ -33,6 +36,18 @@ const actions: ActionTree<State, RootState> = {
     if (data.songs) {
       commit({ type: "setCurrentMusicInfo", song: data.songs[0] });
     }
+  },
+  // 获取当前播放音乐url
+  async getCurrentMusicUrl(
+    { commit },
+    payload: { id: number; options: RequestInit }
+  ) {
+    const res = await fetch(
+      `${baseUrl}/song/url?id=${payload.id}`,
+      payload.options
+    );
+    const data: { code: number; data: Music[] } = await res.json();
+    commit({ type: "setCurrentMusicUrl", url: data.data[0].url });
   },
 };
 
