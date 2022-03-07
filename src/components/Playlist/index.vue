f67f11a629c444842ab5b5d56e0aecae33c2469a
tohsaka888 <156132264@qq.com>
2022-03-07 16:21:50 +0800

    feat: playlist page

---
+++ src/components/Playlist/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>{{ playlist.name }}</div>
+  <div>{{ playlist.id }}</div>
+</template>
+  
+<script setup lang='ts'>
+import { toRef } from 'vue';
+import { useStore } from 'vuex';
+import { PlaylistState } from '../../../store/types';
+
+const store = useStore()
+
+store.dispatch({ type: 'currentPlaylist/getCurrentPlaylistInfo', id: 5396302207 })
+const playlist = toRef<PlaylistState, keyof (PlaylistState)>(store.state.currentPlaylist, "currentPlaylistInfo")
+
+</script>
+  
+<style scoped>
+
+</style>
