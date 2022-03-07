fae0d78f865384c4dc9d7677de2d01cda195dbfd
tohsaka888 <156132264@qq.com>
2022-03-07 21:10:38 +0800

    feat: 音乐播放器

---
+++ src/components/MusicController/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <audio :src="currentMusicInfo.url" autoplay controls />
+</template>
+
+<script lang="ts" setup>
+import { toRef } from "vue";
+import { useStore } from "vuex";
+import { State } from "../../../store/types";
+
+const store = useStore();
+
+const currentMusicInfo = toRef<State, keyof (State)>(store.state.currentMusic, 'currentMusicInfo')
+
+</script>
+
+<style scoped>
+</style>
