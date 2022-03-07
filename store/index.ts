d350f3b664313fddf23a09905dff2bb291c2bedc
tohsaka888 <156132264@qq.com>
2022-03-07 16:21:20 +0800

    feat: store add playlist

--- store/index.ts
+++ store/index.ts
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 import currentMusic from "./modules/CurrentMusic";
+import currentPlaylist from "./modules/CurrentPlayList";
 
 const store = createStore({
   modules: {
     currentMusic,
-  }
+    currentPlaylist,
+  },
 });
 
 export default store;
---
+++ store/modules/CurrentPlayList.ts
@@ -0,0 +1,42 @@
+import { ActionTree, Module, MutationTree } from "vuex";
+import { baseUrl } from "../../config/baseUrl";
+import { Playlist, PlaylistState, RootState } from "../types";
+
+// inititalState
+const state: PlaylistState = {
+  currentPlaylistInfo: {
+    name: "",
+    id: -1,
+    tracks: [],
+    tags: [],
+    coverImgUrl: "",
+  },
+};
+
+// mutations
+const mutations: MutationTree<PlaylistState> = {
+  setCurrentPlaylistInfo(state, payload: { playlist: Playlist }) {
+    state.currentPlaylistInfo = payload.playlist;
+  },
+};
+
+// actions
+const actions: ActionTree<PlaylistState, RootState> = {
+  async getCurrentPlaylistInfo(
+    { commit },
+    payload: { id: number; options?: RequestInit }
+  ) {
+    const res = await fetch(`${baseUrl}/playlist/detail?id=${payload.id}`, payload.options);
+    const data: { code: number; playlist: Playlist } = await res.json();
+    if (data.code === 200) {
+      commit({ type: "setCurrentPlaylistInfo", playlist: data.playlist });
+    }
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+} as Module<PlaylistState, RootState>;
