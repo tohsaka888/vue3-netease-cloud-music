bf79c58303b5b0f75c720bd86c6d9f29a214c613
tohsaka888 <156132264@qq.com>
2022-03-07 20:29:44 +0800

    feat: 单行文本溢出组件

---
+++ src/components/custom/Ellipsis.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ellipsis">{{ text }}</div>
+</template>
+  
+<script setup lang='ts'>
+defineProps({
+  text: String,
+  width: String
+})
+</script>
+  
+<style scoped lang="css">
+.ellipsis {
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+}
+</style>
