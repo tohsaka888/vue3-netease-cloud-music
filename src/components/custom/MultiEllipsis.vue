a3a0cd2bbac42a1707ff7314372bdf1c85ebe151
tohsaka888 <156132264@qq.com>
2022-03-08 19:01:53 +0800

    feat: 多行文本省略

---
+++ src/components/custom/MultiEllipsis.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :style="{ '-webkit-line-clamp': columns }" class="text" :id="id">{{ text }}</div>
+</template>
+  
+<script setup lang='ts'>
+defineProps({
+  columns: Number,
+  text: String,
+  class: String,
+  id: String
+})
+</script>
+  
+<style>
+.text {
+  word-break: break-all;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+}
+</style>
