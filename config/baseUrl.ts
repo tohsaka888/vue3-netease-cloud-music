25e722eaac090a5556f03f2ee3b249a114208e0b
tohsaka888 <156132264@qq.com>
2022-03-08 10:50:42 +0800

    feat: 添加备用主路由

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -1,3 +1,6 @@
 const baseUrl = "https://netease-cloud-music-api-tohsaka888.vercel.app";
+// const baseUrl = "https://music-api.heheda.top";
 
-export { baseUrl };
+const realIP = "&realIP=116.25.146.177";
+
+export { baseUrl, realIP };
