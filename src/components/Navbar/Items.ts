5339c020d0156191416e1ebe8a799521df21a6e9
tohsaka888 <156132264@qq.com>
2022-03-09 20:02:03 +0800

    feat: 导航菜单项

---
+++ src/components/Navbar/Items.ts
@@ -0,0 +1,21 @@
+type Items = {
+  name: string;
+  route: string;
+}[];
+
+const items: Items = [
+  {
+    name: "个性推荐",
+    route: "/",
+  },
+  {
+    name: "歌单",
+    route: "playlist",
+  },
+  {
+    name: "我的",
+    route: "my",
+  },
+];
+
+export default items;
