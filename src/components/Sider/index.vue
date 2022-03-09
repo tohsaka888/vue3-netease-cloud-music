71d2ae2c927880b38862a8ee0736a67b54860b31
tohsaka888 <156132264@qq.com>
2022-03-09 20:02:29 +0800

    feat: initial

---
+++ src/components/Sider/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sider">
+    1111
+  </div>
+</template>
+  
+<script setup lang='ts'>
+</script>
+  
+<style scoped>
+.sider {
+  width: 18vw;
+  height: 100vh;
+}
+</style>
