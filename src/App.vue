f5e1b15721dfa1ec61accf081fe84db6b486776c
tohsaka888 <156132264@qq.com>
2022-03-09 14:07:22 +0800

    feat: 全局样式

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,20 @@ import MusicController from './components/MusicController/index.vue';
 </script>
 
 <template>
-  <router-view />
-  <MusicController />
+  <div class="main-container">
+    <router-view />
+    <MusicController />
+  </div>
 </template>
 
 <style>
 html body {
   margin: 0px;
+}
+
+.main-container {
+  position: relative;
+  width: 100%;
+  height: 100vh;
 }
 </style>
