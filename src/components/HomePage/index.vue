56ca5b7422084a62a957d0d4b91b43d0515c8f06
tohsaka888 <156132264@qq.com>
2022-03-10 17:42:06 +0800

    feat: 主页优化

--- src/components/HomePage/index.vue
+++ src/components/HomePage/index.vue
@@ -1,22 +1,44 @@
 <template>
-  <div>{{ currentMusicInfo }}</div>
+  <div style="margin: 0px 24px;">
+    <el-carousel type="card" height="200px" :initial-index="0">
+      <el-carousel-item
+        v-for="(banner, index) in banners?.banners"
+        :key="index"
+        style="box-shadow: 0px 0px 5px 2px #cecece;border-radius: 10px;"
+      >
+        <el-image :src="banner.imageUrl" class="banner" />
+      </el-carousel-item>
+    </el-carousel>
+    <div class="title">推荐歌曲</div>
+    <div class="title">推荐歌单</div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { toRef } from "vue";
-import { useStore } from "vuex";
-import { State } from "../../../store/types";
+import { ref } from 'vue';
+import { baseUrl } from '../../../config/baseUrl';
 
-const store = useStore();
+const banners = ref<{ code: number; banners: { imageUrl: string }[] }>()
 
-store.dispatch({
-  type: "currentMusic/getCurrentMusicInfo",
-  id: 1824020871,
-});
+const getBanner = async () => {
+  const res = await fetch(`${baseUrl}/banner?type=0`);
+  const data = await res.json()
+  banners.value = data
+}
 
-const currentMusicInfo = toRef<State, keyof (State)>(store.state.currentMusic, 'currentMusicInfo')
+getBanner()
 
 </script>
 
 <style scoped>
+.banner {
+  height: 200px;
+  width: 100%;
+  border-radius: 10px;
+  min-width: 500px;
+}
+
+.title {
+  font-size: 1.2rem;
+}
 </style>
