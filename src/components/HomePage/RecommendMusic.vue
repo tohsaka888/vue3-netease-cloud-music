48dcd5cea894692bfd40b7dc1e5a5dd788b296bc
tohsaka888 <156132264@qq.com>
2022-03-10 17:42:19 +0800

    feat: 推荐歌曲

---
+++ src/components/HomePage/RecommendMusic.vue
@@ -0,0 +1,19 @@
+<template></template>
+  
+<script setup lang='ts'>
+import { ref } from 'vue';
+import { baseUrl } from '../../../config/baseUrl';
+
+const playlists = ref()
+
+const getPlaylist = async () => {
+  const res = await fetch(`${baseUrl}/top/playlist?limit=10&order=new`)
+  const data = await res.json()
+}
+
+getPlaylist()
+
+</script>
+  
+<style>
+</style>
